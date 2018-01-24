e1684e1461290b454c7ecbe1a2798206120b981c
manisudarshan <35294171+manisudarshan@users.noreply.github.com>
2018-01-24 16:11:15 +0530

    greatest
    program

---
+++ largest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("\n enter the numbers");
+scanf("%d,%d,%d",&a,&b,&c);
+if((a>b)&&(a>b))
+printf("\n %d is the greatest number",a);
+else
+{
+if((b>c)&&(b>a))
+printf("\n %d is the greatest number",b);
+else
+printf("\n%d is the greatest number",c);
+}
+getch();
+}
