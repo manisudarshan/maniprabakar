3efea28cd709bc3ed700a3bed98100e81ffb53c5
manisudarshan <35294171+manisudarshan@users.noreply.github.com>
2018-01-24 16:51:07 +0530

    program
    program

---
+++ digits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num,count=0;
+printf("\n enter a number:");
+scanf("%d",&num);
+while(num!=0)
+{
+count=count+1;
+}
+printf("\n total number of digits present are %d",count);
+getch();
+}
