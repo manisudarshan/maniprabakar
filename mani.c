cff0f7a415b0ea1529f81b011ca76e98602403cf
manisudarshan <35294171+manisudarshan@users.noreply.github.com>
2018-01-24 16:48:05 +0530

    sum
    program

--- mani.c
+++ mani.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main()
+void main()
 {
 int n[7]={7,6,5,4,3,2,1},i,sum=0,k;
   printf("Enter the value of k");
@@ -9,5 +9,5 @@ int n[7]={7,6,5,4,3,2,1},i,sum=0,k;
     sum=sum+n[i];
   }
   printf("The num is %d",sum);
-return 0;
+getch();
 }
